8fecd81b6f939757e3ce4a4331befed0881e8da6
Meng <meng.li@eroad.com>
2017-06-14 16:40:08 +1200

    v1.0.5

--- es/index.js
+++ es/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import R from 'ramda';
 
 export var name = 'foobar';
 
 export default React.createClass({
   displayName: 'src',
   render: function render() {
+    R.add(2, 3);
     return React.createElement(
       'div',
       null,
