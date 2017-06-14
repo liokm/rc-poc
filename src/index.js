e3c9dbbf1cda5eb3ec8c228e89bab0a590fac6d7
Meng <meng.li@eroad.com>
2017-06-14 16:38:53 +1200

    Explicitly add dependency ramda

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import R from 'ramda';
 
 export const name = 'foobar';
 
 export default React.createClass({
   render() {
+    R.add(2, 3);
     return <div>
       <h2>Welcome to React components!</h2>
     </div>
